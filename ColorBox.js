887e352813e8f233636a0693bcad1d6c34479377
Veselin <vesselin60@gmail.com>
2022-01-25 08:46:06 +0200

    component template

---
+++ ColorBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Component = () => {
+    return (
+        <View>
+            <Text>Component</Text>
+        </View>
+    );
+}; 
+
+const styles = StyleSheet.create({});
+
+export default Component;
